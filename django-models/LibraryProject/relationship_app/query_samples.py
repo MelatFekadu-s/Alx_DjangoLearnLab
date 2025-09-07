6f33c85f386fb7b177addca3a5e2fc3bdc2ae34f
bemnet-21 <anjejobemnet@gmail.com>
2025-09-07 21:19:46 +0300

    django

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,10 @@
+from .models import Author, Book, Library, Librarian
+
+def books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
+
+def books_in_library(library_name):
+    return Library.objects.get(name=library_name).books.all()
+
+def librarian_for_library(library_name):
+    return Library.objects.get(name=library_name).librarian
