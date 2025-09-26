ed2d5241bf413ebc8b6e9603f925c9992628e955
MelatFekadu-s <ejetamelat@gmail.com>
2025-09-26 23:00:27 +0300

    django

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -29,8 +29,13 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 INSTALLED_APPS = [
+    'rest_framework',
     'api',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,8 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-}
+
