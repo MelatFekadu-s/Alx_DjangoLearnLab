dbe2478a9775512e2d2726390c6269cf90547b9e
MelatFekadu-s <ejetamelat@gmail.com>
2025-09-26 22:46:02 +0300

    django

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,13 @@
 from rest_framework.generics import ListAPIView
+from rest_framework import viewsets  # <-- added import
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-
+    permission_classes = [IsAuthenticated] 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
